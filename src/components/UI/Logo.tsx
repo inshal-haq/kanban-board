ad077ff4164336e203e5361adc8557a93208c8b1
Inshal Haq <inshalulhaq11@gmail.com>
2024-04-28 18:09:15 -0500

    Update kanban icon and page title

--- src/components/UI/Logo.tsx
+++ src/components/UI/Logo.tsx
@@ -1,10 +1,9 @@
-import { PiKanbanFill } from "react-icons/pi";
-// import { PiKanbanLight } from "react-icons/pi";
+import kanbanIcon from "../../assets/kanban-fill-purple.svg";
 
 const Logo: React.FC = () => {
   return (
     <div className="border-r-lines-light flex items-center justify-between gap-3 border-r-2 px-7 py-5 text-4xl font-bold">
-      <PiKanbanFill />
+      <img src={kanbanIcon} alt="kanban icon" width={36} />
       kanban
     </div>
   );
