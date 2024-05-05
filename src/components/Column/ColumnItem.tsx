16d101cd69eb2c4627ff49937f542c440c9db164
Inshal Haq <inshalulhaq11@gmail.com>
2024-05-05 17:19:11 -0500

    Add colored dots on ColumnItem

--- src/components/Column/ColumnItem.tsx
+++ src/components/Column/ColumnItem.tsx
@@ -1,13 +1,36 @@
 import TaskList from "../Task/TaskList";
 import Column from "../../models/column";
 
-const ColumnItem: React.FC<{ column: Column }> = (props) => {
+const ColumnItem: React.FC<{ column: Column; index: number }> = (props) => {
   const { name, tasks } = props.column;
+
+  let color = "";
+  switch ((props.index + 1) % 5) {
+    case 0:
+      color = "bg-[#49C4E5]";
+      break;
+    case 1:
+      color = "bg-[#8471F2]";
+      break;
+    case 2:
+      color = "bg-[#67E2AE]";
+      break;
+    case 3:
+      color = "bg-[#CE4760]";
+      break;
+    case 4:
+      color = "bg-[#FAFF7F]";
+      break;
+
+    default:
+      color = "bg-[#49C4E5]";
+      break;
+  }
 
   return (
     <div className="h-max w-[280px]">
       <div className="mb-4 flex items-center gap-2">
-        <div className="h-3 w-3 rounded-full bg-black"></div>
+        <div className={`h-3 w-3 rounded-full ${color}`}></div>
         <h4 className="text-medium-gray">
           {name.toUpperCase()} ({tasks.length})
         </h4>
--- src/components/Column/ColumnList.tsx
+++ src/components/Column/ColumnList.tsx
@@ -4,8 +4,8 @@ import Column from "../../models/column";
 const ColumnList: React.FC<{ columns: Column[] }> = (props) => {
   return (
     <ul className="flex gap-6">
-      {props.columns.map((column) => (
-        <ColumnItem key={column.id} column={column} />
+      {props.columns.map((column, index) => (
+        <ColumnItem key={column.id} column={column} index={index} />
       ))}
       <h1 className="mt-8 flex w-[280px] items-center justify-center rounded-lg bg-[#E9EFFA] text-medium-gray">
         + New Column
