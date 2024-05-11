3c105350582b538dc4c9fd828748041fd9a0d0af
Inshal Haq <inshalulhaq11@gmail.com>
2024-05-11 17:41:29 -0500

    Complete TextField and add useInput hook

--- src/components/Task/AddTaskModal.tsx
+++ src/components/Task/AddTaskModal.tsx
@@ -1,16 +1,53 @@
 import DialogModal from "../UI/DialogModal";
 import TextField from "../UI//TextField";
+import { useInput } from "../../hooks/useInput";
 
 const AddTaskModal: React.FC<{
   open: boolean;
   setOpen: (state: boolean) => void;
 }> = (props) => {
+  const isNotEmpty = (value: string) => value.trim() !== "";
+
+  const {
+    value: titleValue,
+    handleInputChange: handleTitleChange,
+    handleInputBlur: handleTitleBlur,
+    hasError: titleHasError,
+  } = useInput("", isNotEmpty);
+  const {
+    value: descriptionValue,
+    handleInputChange: handleDescriptionChange,
+    handleInputBlur: handleDescriptionBlur,
+    hasError: descriptionHasError,
+  } = useInput("", isNotEmpty);
+
   return (
     <DialogModal open={props.open} setOpen={props.setOpen}>
       <h2>Add New Task</h2>
       <label className="text-medium-gray">
         <p className="mb-2">Title</p>
-        <TextField />
+        <TextField
+          id="title"
+          name="title"
+          placeholder="e.g. Take coffee break"
+          value={titleValue}
+          onChange={handleTitleChange}
+          onBlur={handleTitleBlur}
+          error={titleHasError}
+        />
+      </label>
+      <label className="text-medium-gray">
+        <p className="mb-2">Description</p>
+        <TextField
+          id="description"
+          name="description"
+          textarea
+          placeholder="e.g. It’s always good to take a break. This 15 minute break will recharge the batteries a little."
+          value={descriptionValue}
+          onChange={handleDescriptionChange}
+          onBlur={handleDescriptionBlur}
+          error={descriptionHasError}
+        />
       </label>
     </DialogModal>
   );
--- src/components/UI/TextField.tsx
+++ src/components/UI/TextField.tsx
@@ -1,10 +1,28 @@
-const TextField = () => {
-  const error = false;
-
+const TextField = ({ id, error, textarea, ...props }) => {
   return (
-    <input
-      className={`border-1 w-full rounded border border-solid focus:outline-none ${error ? "border-red" : "border-lines-light"} px-4 py-2`}
-    />
+    <div className="relative">
+      {textarea && (
+        <textarea
+          id={id}
+          {...props}
+          className={`border-1 w-full rounded border border-solid text-black placeholder:text-black placeholder:text-opacity-25 focus:outline-none ${error ? "border-red" : "border-lines-light"} h-24 resize-none px-4 py-2`}
+        />
+      )}
+      {!textarea && (
+        <input
+          id={id}
+          {...props}
+          className={`border-1 w-full rounded border border-solid text-black placeholder:text-black placeholder:text-opacity-25 focus:outline-none ${error ? "border-red" : "border-lines-light"} px-4 py-2`}
+        />
+      )}
+      {error && (
+        <div
+          className={`absolute ${textarea ? "bottom-4" : "bottom-2"} right-4 text-red`}
+        >
+          Can't be empty!
+        </div>
+      )}
+    </div>
   );
 };
 
